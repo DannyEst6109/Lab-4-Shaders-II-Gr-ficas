3aba1adcdb3e3b5c750b79c62a6e755f711bb31c
DannyEst6109 <72481675+DannyEst6109@users.noreply.github.com>
2022-11-09 21:42:10 -0600

    Add files via upload

--- gl.py
+++ gl.py
@@ -1,19 +1,15 @@
-import glm #pip install PyGLM
-
-from numpy import array, float32
-
-# pip install PyOpenGL
+import glm
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
-
-from obj import Obj
-
 from pygame import image
+from numpy import array, float32
 
+import obj
 
 class Model(object):
     def __init__(self, objName, textureName):
-        self.model = Obj(objName)
+
+        self.model = obj.Obj(objName)
 
         self.createVertexBuffer()
 
@@ -26,14 +22,27 @@ class Model(object):
         self.texture = glGenTextures(1)
 
 
+    def getModelMatrix(self):
+        identity = glm.mat4(1)
+
+        translateMatrix = glm.translate(identity, self.position)
+
+        pitch = glm.rotate(identity, glm.radians( self.rotation.x ), glm.vec3(1,0,0) )
+        yaw   = glm.rotate(identity, glm.radians( self.rotation.y ), glm.vec3(0,1,0) )
+        roll  = glm.rotate(identity, glm.radians( self.rotation.z ), glm.vec3(0,0,1) )
+        rotationMatrix = pitch * yaw * roll
+
+        scaleMatrix = glm.scale(identity, self.scale)
+
+        return translateMatrix * rotationMatrix * scaleMatrix
+
+
+
     def createVertexBuffer(self):
+
         buffer = []
 
-        self.polycount = 0
-
         for face in self.model.faces:
-            self.polycount += 1
-
             for i in range(3):
                 # positions
                 pos = self.model.vertices[face[i][0] - 1]
@@ -41,111 +50,68 @@ class Model(object):
                 buffer.append(pos[1])
                 buffer.append(pos[2])
 
-                # texcoords
-                uvs = self.model.texcoords[face[i][1] - 1]
-                buffer.append(uvs[0])
-                buffer.append(uvs[1])
-
                 # normals
                 norm = self.model.normals[face[i][2] - 1]
                 buffer.append(norm[0])
                 buffer.append(norm[1])
                 buffer.append(norm[2])
 
-            if len(face) == 4:
-
-                self.polycount += 1
-
-                for i in [0,2,3]:
-                    # positions
-                    pos = self.model.vertices[face[i][0] - 1]
-                    buffer.append(pos[0])
-                    buffer.append(pos[1])
-                    buffer.append(pos[2])
-
-                    # texcoords
-                    uvs = self.model.texcoords[face[i][1] - 1]
-                    buffer.append(uvs[0])
-                    buffer.append(uvs[1])
-
-                    # normals
-                    norm = self.model.normals[face[i][2] - 1]
-                    buffer.append(norm[0])
-                    buffer.append(norm[1])
-                    buffer.append(norm[2])
-
+                # texCoords
+                uvs = self.model.texcoords[face[i][1] - 1]
+                buffer.append(uvs[0])
+                buffer.append(uvs[1])
 
         self.vertBuffer = array(buffer, dtype = float32)
-        
-        # Vertex Buffer Object
-        self.VBO = glGenBuffers(1)
-        # Vertex Array Object
-        self.VAO = glGenVertexArrays(1)
 
-    def getModelMatrix(self):
-        identity = glm.mat4(1)
+        self.VBO = glGenBuffers(1) #Vertex Buffer Object
+        self.VAO = glGenVertexArrays(1) #Vertex Array Object
 
-        translateMat = glm.translate(identity, self.position)
+    def renderInScene(self):
 
-        pitch = glm.rotate(identity, glm.radians(self.rotation.x), glm.vec3(1,0,0))
-        yaw   = glm.rotate(identity, glm.radians(self.rotation.y), glm.vec3(0,1,0))
-        roll  = glm.rotate(identity, glm.radians(self.rotation.z), glm.vec3(0,0,1))
-
-        rotationMat = pitch * yaw * roll
-
-        scaleMat = glm.scale(identity, self.scale)
-
-        return translateMat * rotationMat * scaleMat
-
-    def render(self):
-
-        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
         glBindVertexArray(self.VAO)
+        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
 
-        # Mandar la informacion de vertices
-        glBufferData(GL_ARRAY_BUFFER,           # Buffer ID
-                     self.vertBuffer.nbytes,    # Buffer size in bytes
-                     self.vertBuffer,           # Buffer data
-                     GL_STATIC_DRAW)            # Usage
+        # Los vertices
+        glBufferData(GL_ARRAY_BUFFER,           #Buffer ID
+                     self.vertBuffer.nbytes,    #Buffer size in bytes
+                     self.vertBuffer,           #Buffer data
+                     GL_STATIC_DRAW )           #Usage
 
-        # Atributos
-
-        # Atributo de posiciones
+        # Atributo de posicion
         glVertexAttribPointer(0,                # Attribute number
                               3,                # Size
                               GL_FLOAT,         # Type
-                              GL_FALSE,         # Is it normalized
+                              GL_FALSE,         # It it normalized?
                               4 * 8,            # Stride
-                              ctypes.c_void_p(0))# Offset
+                              ctypes.c_void_p(0)) # Offset
 
         glEnableVertexAttribArray(0)
 
-        # Atributo de texcoords
-        glVertexAttribPointer(1,                # Attribute number
-                              2,                # Size
-                              GL_FLOAT,         # Type
-                              GL_FALSE,         # Is it normalized
-                              4 * 8,            # Stride
-                              ctypes.c_void_p(4*3))# Offset
+        # Atributo de normales
+        glVertexAttribPointer(1,
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              4 * 8,
+                              ctypes.c_void_p(4 * 3))
 
         glEnableVertexAttribArray(1)
 
-        # Atributo de normals
-        glVertexAttribPointer(2,                # Attribute number
-                              3,                # Size
-                              GL_FLOAT,         # Type
-                              GL_FALSE,         # Is it normalized
-                              4 * 8,            # Stride
-                              ctypes.c_void_p(4*5))# Offset
+
+        # Atributo de coordenadas de textura
+        glVertexAttribPointer(2,
+                              2,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              4 * 8,
+                              ctypes.c_void_p(4 * 6))
 
         glEnableVertexAttribArray(2)
 
-
-        # Dar la textura
-        glActiveTexture( GL_TEXTURE0 )
+        # Dar textura
         glBindTexture(GL_TEXTURE_2D, self.texture)
-        glTexImage2D(GL_TEXTURE_2D,                     # Texture Type
-                     0,                                 # Positions
+        glTexImage2D(GL_TEXTURE_2D,                     # Texture type
+                     0,                                 # Level
                      GL_RGB,                            # Format
                      self.textureSurface.get_width(),   # Width
                      self.textureSurface.get_height(),  # Height
@@ -158,7 +124,8 @@ class Model(object):
 
 
 
-        glDrawArrays(GL_TRIANGLES, 0, self.polycount * 3 )
+        # Dibujar
+        glDrawArrays(GL_TRIANGLES, 0, len(self.model.faces) * 3 ) # Para dibujar vertices en orden
 
 
 class Renderer(object):
@@ -169,93 +136,81 @@ class Renderer(object):
         glEnable(GL_DEPTH_TEST)
         glViewport(0,0, self.width, self.height)
 
-        self.filledMode()
-
         self.scene = []
-        self.active_shader = None
 
-        self.pointLight = glm.vec3(0,0,0)
-        self.time = 0
-        self.value = 0;
+        self.pointLight = glm.vec3(-10, 0, -5)
 
-        self.target = glm.vec3(0,0,0)
-        self.angle = 0
-        self.camDistance = 5
+        self.tiempo = 0
+        self.valor = 0
 
-        # ViewMatrix
+        # View Matrix
         self.camPosition = glm.vec3(0,0,0)
-        self.camRotation = glm.vec3(0,0,0)
-        self.viewMatrix = self.getViewMatrix()
+        self.camRotation = glm.vec3(0,0,0) # pitch, yaw, roll
 
         # Projection Matrix
-        self.projectionMatrix = glm.perspective(glm.radians(60),        # FOV
-                                                self.width/self.height, # Aspect Ratio
-                                                0.1,                    # Near Plane
-                                                1000)                   # Far Plane
-        
+        self.projectionMatrix = glm.perspective(glm.radians(60),            # FOV en radianes
+                                                self.width / self.height,   # Aspect Ratio
+                                                0.1,                        # Near Plane distance
+                                                1000)                       # Far plane distance
 
-
-
-    def filledMode(self):
-        glPolygonMode(GL_FRONT, GL_FILL)
-
-    def wireframeMode(self):
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
 
     def getViewMatrix(self):
         identity = glm.mat4(1)
 
-        translateMat = glm.translate(identity, self.camPosition)
+        translateMatrix = glm.translate(identity, self.camPosition)
 
-        pitch = glm.rotate(identity, glm.radians(self.camRotation.x), glm.vec3(1,0,0))
-        yaw   = glm.rotate(identity, glm.radians(self.camRotation.y), glm.vec3(0,1,0))
-        roll  = glm.rotate(identity, glm.radians(self.camRotation.z), glm.vec3(0,0,1))
+        pitch = glm.rotate(identity, glm.radians( self.camRotation.x ), glm.vec3(1,0,0) )
+        yaw   = glm.rotate(identity, glm.radians( self.camRotation.y ), glm.vec3(0,1,0) )
+        roll  = glm.rotate(identity, glm.radians( self.camRotation.z ), glm.vec3(0,0,1) )
 
-        rotationMat = pitch * yaw * roll
+        rotationMatrix = pitch * yaw * roll
 
-        camMatrix = translateMat * rotationMat
+        camMatrix = translateMatrix * rotationMatrix
 
         return glm.inverse(camMatrix)
 
 
-    def setShaders(self, vertexShader, fragmentShader):
-        if vertexShader is not None and fragmentShader is not None:
+    def wireframeMode(self):
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+    def filledMode(self):
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
+
+    def setShaders(self, vertexShader, fragShader):
+        if vertexShader is not None and fragShader is not None:
             self.active_shader = compileProgram( compileShader(vertexShader, GL_VERTEX_SHADER),
-                                                 compileShader(fragmentShader, GL_FRAGMENT_SHADER))
+                                                 compileShader(fragShader, GL_FRAGMENT_SHADER))
         else:
             self.active_shader = None
 
-    def update(self):
-        #self.viewMatrix = self.getViewMatrix()
-
-        self.viewMatrix = glm.lookAt(self.camPosition, self.target, glm.vec3(0,1,0))
-
-
 
     def render(self):
-        glClearColor(0.2,0.2,0.2, 1)
+        glClearColor(0.2,0.2,0.2,1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        if self.active_shader is not None:
-            glUseProgram(self.active_shader)
+        glUseProgram(self.active_shader)
 
-            glUniformMatrix4fv( glGetUniformLocation(self.active_shader, "viewMatrix"),
-                                1, GL_FALSE, glm.value_ptr(self.viewMatrix))
+        if self.active_shader:
+            glUniformMatrix4fv(glGetUniformLocation(self.active_shader, "viewMatrix"),
+                               1, GL_FALSE, glm.value_ptr(self.getViewMatrix()))
 
-            glUniformMatrix4fv( glGetUniformLocation(self.active_shader, "projectionMatrix"),
-                                1, GL_FALSE, glm.value_ptr(self.projectionMatrix))
+            glUniformMatrix4fv(glGetUniformLocation(self.active_shader, "projectionMatrix"),
+                               1, GL_FALSE, glm.value_ptr(self.projectionMatrix))
 
-            glUniform1i( glGetUniformLocation(self.active_shader, "tex"), 0)
+            glUniform1f(glGetUniformLocation(self.active_shader, "tiempo"), self.tiempo)
+            glUniform1f(glGetUniformLocation(self.active_shader, "valor"), self.valor)
 
-            glUniform1f( glGetUniformLocation(self.active_shader, "time"), self.time)
-
-            glUniform3fv( glGetUniformLocation(self.active_shader, "pointLight"), 1, glm.value_ptr(self.pointLight))
-
-
-        for obj in self.scene:
-            if self.active_shader is not None:
-                glUniformMatrix4fv( glGetUniformLocation(self.active_shader, "modelMatrix"),
-                                    1, GL_FALSE, glm.value_ptr(obj.getModelMatrix()))
+            glUniform3f(glGetUniformLocation(self.active_shader, "pointLight"),
+                        self.pointLight.x, self.pointLight.y, self.pointLight.z)
 
 
-            obj.render()
+        for model in self.scene:
+            if self.active_shader:
+                glUniformMatrix4fv(glGetUniformLocation(self.active_shader, "modelMatrix"),
+                                   1, GL_FALSE, glm.value_ptr(model.getModelMatrix()))
+
+            model.renderInScene()
+
+
+            
--- shaders.py
+++ shaders.py
@@ -1,48 +1,89 @@
-vertex_shader ='''
-#version 450 core
-
+vertex_shader = """
+#version 450
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 texcoords;
-layout (location = 2) in vec3 normals;
-
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 texCoords;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-
-uniform float time;
-
-out vec2 UVs;
-out vec3 norms;
-out vec3 pos;
-
-void main()
-{
-    UVs = texcoords;
-    norms = normals;
-    pos = (modelMatrix * vec4(position + normals * sin(time * 3)/10, 1.0)).xyz;
-
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position + normals * sin(time * 3)/10, 1.0);
-
-}
-'''
-
-fragment_shader ='''
-#version 450 core
-
-out vec4 fragColor;
-
-in vec2 UVs;
-in vec3 norms;
-in vec3 pos;
-
+uniform float tiempo;
+uniform float valor;
 uniform vec3 pointLight;
-
-uniform sampler2D tex;
-
+out vec3 outColor;
+out vec2 outTexCoords;
 void main()
 {
-    float intensity = dot(norms, normalize(pointLight - pos));
-
-    fragColor = texture(tex, UVs) * intensity;
+    vec4 norm = vec4(normal, 0.0);
+    vec4 pos = vec4(position, 1.0) + norm * valor;
+    pos = modelMatrix * pos;
+    vec4 light = vec4(pointLight, 1.0);
+    float intensity = dot(modelMatrix * norm, normalize(light - pos));
+    gl_Position = projectionMatrix * viewMatrix * pos;
+    outColor = vec3(1.0,1.0 - valor * 2,1.0-valor * 2) * intensity;
+    outTexCoords = texCoords;
 }
-'''
+"""
+
+
+fragment_shader = """
+#version 450
+layout (location = 0) out vec4 fragColor;
+in vec3 outColor;
+in vec2 outTexCoords;
+uniform sampler2D tex;
+void main()
+{
+    fragColor = vec4(outColor, 1) * texture(tex, outTexCoords);
+}
+"""
+
+
+#toon shader basado en shader de modulo 1 del curso
+toon_shader = """
+
+uniform vec3 lightDir;
+varying vec3 normal;
+out vec3 outColor;
+
+
+void main(){
+    float intensity;
+    intensity = dot(lightDir, normalize(normal));
+    
+    if intensity > 0.7
+        intensity = 1
+    if intensity > 0.3
+        intensity = 0.5
+    else
+        intensity = 0.5
+        
+    outColor = outColor * intensity
+}
+
+"""
+
+shader= """
+
+uniform vec3 lightDir;
+varying vec3 normal;
+
+void main(){
+    float intensity;
+    intensity = dot(lightDir, normalize(normal));
+    
+    if (intensity > 0.80)
+       
+        
+    if (intensity > 0.40)
+        
+        
+    if (intensity > 0.15)
+        
+        
+    else
+        
+    
+}
+
+"""
+
